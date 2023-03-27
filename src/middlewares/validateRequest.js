a8a9604b878f7467982135be90a4a70ccb4b82a6
nsnhatlong <nsnl.coder@gmail.com>
2023-03-27 18:49:04 -0400

    1.0.9

--- src/middlewares/validateRequest.js
+++ src/middlewares/validateRequest.js
@@ -16,7 +16,6 @@ const validateRequest = (schema) => async (req, res, next) => {
     return next();
   } catch (err) {
     let errors = err.errors;
-    console.log(err);
 
     if (process.env.NODE_ENV === 'development') {
       errors = err;
